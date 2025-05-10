a68a0a7ad324c87794db1d8b817a1f944ba39caa
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-10 15:39:05 +0600

    add button

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,21 +1,21 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
-
+import "./Countries.css";
 const Countries = ({ countriesDada }) => {
   const useCountriesDada = use(countriesDada);
   //   console.log(useCountriesDada);
   return (
-    <div
-      style={{
-        color: "blue",
-      }}
-    >
-      <h2>Traveling Countries : {useCountriesDada.length}</h2>
-      {useCountriesDada.map((country) => (
-        <Country key={country.cca3} country={country}>
-          {" "}
-        </Country>
-      ))}
+    <div>
+      <h2 style={{ textAlign: "center" }}>
+        Traveling Countries : {useCountriesDada.length}
+      </h2>
+      <div className="countries">
+        {useCountriesDada.map((country) => (
+          <Country key={country.cca3} country={country}>
+            {" "}
+          </Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./Country.css";
 const Country = ({ country }) => {
-  console.log(country.flags);
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    // setVisited(true)
+    if (visited === true) {
+      setVisited(false);
+    } else {
+      setVisited(true);
+    }
+  };
+  //   console.log(country);
   return (
-    <div>
+    <div className={`country ${visited && "country-visited"}`}>
       <h3>Country Name : {country.name.common} </h3>
       <img src={country.flags.png} alt="" />
       <p>Independent : {country.independent ? "Free" : "Not Free"}</p>
       <p>population " {country.population}</p>
+      <button onClick={handleVisited}>
+        {visited ? "visited" : "Not Visited"}
+      </button>
       {/* <p>
         <strong>official</strong> : {country.name.official}
       </p> */}
